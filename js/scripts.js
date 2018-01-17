bb62e9713cceaa926d2fe339022b7d34f84dcf7a
Josh Rochon <joshdrochon@gmail.com>
2018-01-16 18:57:45 -0800

    use for loop to cycle through and array of card ranks and write their suits to the document

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var ranks = ["ace",2,3,4,5,6,7,8,9,10, "jack", "queen", "king"];
+
+for (i = 0; i < ranks.length; i++) {
+    document.write(ranks[i] + " of spades, diamonds, hearts, clubs" + "<br>");
+}
